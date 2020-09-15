9735da655a321d299761edc0d94950bb23d8c849
timrinkerman <tim.rinkerman98@gmail.com>
2020-09-15 14:21:45 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+
+export default class Keypad extends Component {
+
+
+    keyUpHandler = () => console.log("Entering password...");
+        // prints either LoginInput or PwdInput
+    
+
+render(){
+    return(
+        <div>
+            <input type="password" 
+            onKeyUp={this.keyUpHandler}/>
+            </div>
+        )
+}
+
+
+}
